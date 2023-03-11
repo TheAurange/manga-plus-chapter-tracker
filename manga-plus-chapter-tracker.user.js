9215d4cb5c7a2e12c73055191934ccfd2e68df77
Aurange <11928004+TheAurange@users.noreply.github.com>
2023-03-11 17:04:09 -0600

    Create

---
+++ manga-plus-chapter-tracker.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name    [Manga Plus] Chapter Tracker
+// @author  Aurange
+// @version 1.0
+// @match   https://mangaplus.shueisha.co.jp/titles/*
+// @grant   window.close
+// ==/UserScript==
+
+"use strict";
+
+new MutationObserver(function(mutationList, observer){
+  let c = document.querySelectorAll("div[class^='ChapterListItem-module_chapterListItem']")[document.querySelectorAll("div[class^='ChapterListItem-module_chapterListItem']").length - 1];
+
+  if(c){
+    observer.disconnect();
+
+    let cT = document.querySelector("h1").innerText,
+        cN = c.children[0].children[2].innerText.split("Chapter ")[1].split(":")[0];
+
+    if(localStorage.getItem(cT) === null) localStorage.setItem(cT, cN);
+    else if(Number(cN) > Number(localStorage.getItem(cT))){
+      localStorage.setItem(cT, cN);
+
+      window.location.href = "https://mangaplus.shueisha.co.jp/viewer/" + c.children[0].children[0].dataset.src.split("chapter/")[1].split("/chapter")[0];
+    }
+    else window.close();
+  }
+}).observe(document, {
+  childList: true,
+  subtree: true
+});
